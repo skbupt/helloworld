3bc694a17a06ccac13a463e4e92e6bc4c64ede2f
Taoz <zhangtao@neotel.com.cn>
2018-06-20 15:44:54 +0800

    init

---
+++ src/main/scala/com/neo/sk/helloworld/Boot.scala
@@ -0,0 +1,14 @@
+package com.neo.sk.helloworld
+
+/**
+  * User: Taoz
+  * Date: 6/20/2018
+  * Time: 3:37 PM
+  */
+object Boot {
+
+  def main(args: Array[String]): Unit = {
+    println("hello world.")
+  }
+
+}
